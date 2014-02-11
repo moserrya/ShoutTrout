074f2b1fd3acdba431aba3d520553003c600c83f
Ryan Moser <ryanpmoser@gmail.com>
2014-02-10 21:21:45 -0800

    [FEATURE] create cancel subscription worker

---
+++ lib/cancel_subscription.rb
@@ -0,0 +1,8 @@
+class CancelSubscription
+  include Sidekiq::Worker
+
+  def perform(stripe_customer_token)
+    customer = Stripe::Customer.retrieve(stripe_customer_token)
+    customer.cancel_subscription({at_period_end: true})
+  end
+end
