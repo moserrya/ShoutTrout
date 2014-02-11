285727ceeae113995b1c7d1ce330832e2e61f195
Ryan Moser <ryanpmoser@gmail.com>
2014-02-10 21:20:40 -0800

    [FEATURE] add outbound message worker

--- lib/outbound_messager.rb
+++ lib/outbound_messager.rb
@@ -1,6 +1,17 @@
 class OutboundMessager
   include Sidekiq::Worker
   include PlivoClient
-end
 
-# pivlo sms url: shouttrout.com/inbound_messages
+  def perform
+    send_daily_messages
+  end
+
+  private
+  def send_daily_messages
+    User.transaction do
+      hourly_users = User.joins(:subscription).merge(Subscription.active).where(hour_to_send_message: DateTime.now.hour)
+      send_messages(hourly_users.pluck(:phone_number))
+      hourly_users.update_all(last_outbound_message_at: DateTime.now)
+    end
+  end
+end
