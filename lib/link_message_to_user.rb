418dd21f1014b57e578d1f786d370a46fb40a6cf
Ryan Moser <ryanpmoser@gmail.com>
2014-02-17 22:47:34 -0800

    [BUG] fix delegation issue in message linker

--- lib/link_message_to_user.rb
+++ lib/link_message_to_user.rb
@@ -1,8 +1,7 @@
 class LinkMessageToUser < Struct.new(:message)
-  delegate :contact, to: :user
-
   def call
     if user = User.includes(:contact).find_by(phone_number: message.from)
+      contact = user.contact
       contact.update notified_at: nil if contact.notified?
       message.user = user
     end
