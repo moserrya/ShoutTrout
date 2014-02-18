215b87f3a204a431fd252c28a9ed3778bed8de96
Ryan Moser <ryanpmoser@gmail.com>
2014-02-17 22:47:03 -0800

    [REFACTOR] clean up daily message generation

--- lib/daily_message_worker.rb
+++ lib/daily_message_worker.rb
@@ -12,11 +12,13 @@ class DailyMessageWorker
   private
   def send_daily_messages
     hourly_users = User.active.where(hour_to_send_message: Time.now.hour)
-    send_messages(hourly_users.pluck(:phone_number), build_daily_message)
+    send_messages(hourly_users.pluck(:phone_number), message_body)
     hourly_users.update_all(last_outbound_message_at: Time.now)
   end
 
-  def build_daily_message
-    "Hi, this is #{['Tiny', 'Big', 'Rainbow'].sample} Trout. Happy #{Date::DAYNAMES[DateTime.now.cwday]}! #{['Please let me know that', 'Hope'].sample} you got this. #{['Can\'t wait to hear', 'Looking forward to hearing'].sample} from you #{[':)', ].sample}"
+  def message_body
+    message = "Hi, this is #{['Tiny', 'Big', 'Rainbow'].sample} Trout. Happy #{Date::DAYNAMES[DateTime.now.cwday]}! "
+    message << "#{['Please let me know that', 'Hope'].sample} you got this. "
+    message << "#{['Can\'t wait to hear', 'Looking forward to hearing'].sample} from you #{[':)', 'ᕕ( ᐛ )ᕗ'].sample}"
   end
 end
