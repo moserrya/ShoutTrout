bfdad787e7a4b0175cc30b401c07ccb16912f838
Ryan Moser <ryanpmoser@gmail.com>
2014-02-17 18:07:31 -0800

    [FEATURE] create seed data using faker to test performance

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+USERS_TO_CREATE = 10_000
+
+USERS_TO_CREATE.times do |i|
+  puts "Creating user #{i + 1} out of #{USERS_TO_CREATE}" if (i % 100).zero?
+
+  user = User.create phone_number: Faker::Number.number(10),
+                    email: Faker::Internet.email,
+                    password: Faker::Internet.password,
+                    name: Faker::Name.name,
+                    last_outbound_message_at: Time.now,
+                    last_inbound_message_at: rand(27).hours.ago,
+                    hour_to_send_message: 20 - rand(12)
+
+  next unless user.persisted?
+
+  user.create_subscription active: rand(4).zero? ? false : true
+
+  user.create_contact phone_number: Faker::Number.number(10),
+                      name: Faker::Name.name
+
+  rand(100).times { user.inbound_messages.create from: user.phone_number, to: '+17039442823', text: Faker::Lorem.characters(rand 160)}
+end
