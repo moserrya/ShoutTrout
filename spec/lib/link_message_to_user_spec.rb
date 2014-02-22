ea4fe0c633502f777127e97087ebc6826ef1a3bb
Ryan Moser <ryanpmoser@gmail.com>
2014-02-21 23:52:43 -0800

    [TEST] add LinkMessageToUser specs

--- spec/lib/link_message_to_user_spec.rb
+++ spec/lib/link_message_to_user_spec.rb
@@ -2,16 +2,19 @@ require 'spec_helper'
 
 describe LinkMessageToUser do
 
-  let(:user)    { OpenStruct.new phone_number: '17039442823', id: 33}
-  let(:message) { OpenStruct.new from: user.phone_number }
-
-  before :each do
-    User.stub(:find_by).with(phone_number: user.phone_number).and_return(user)
-  end
+  let(:contact) {FactoryGirl.create :contact, notified_at: 6.hours.ago}
+  let(:user) { contact.user}
+  let(:message) {FactoryGirl.build :inbound_message, from: user.phone_number}
 
   it 'sets the user_id of the message based on the phone number' do
     expect {
       LinkMessageToUser.new(message).call
     }.to change{message.user_id}.from(nil).to(user.id)
+  end
+
+  it 'marks contacts as no longer notified' do
+    expect {
+      LinkMessageToUser.new(message).call
+    }.to change{contact.reload.notified?}.from(true).to(false)
   end
 end
