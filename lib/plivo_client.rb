960ef1030874a80c7c11a0ded31422746438b840
Ryan Moser <ryanpmoser@gmail.com>
2014-02-22 15:05:02 -0800

    [CHORE] raise error in plivo client on a non-200 status code

--- lib/plivo_client.rb
+++ lib/plivo_client.rb
@@ -7,7 +7,9 @@ module PlivoClient
 
   def send_messages(dsts, body)
     dst_list = dsts.join('<')
-    plivo.send_message message_params(dst_list, body)
+    status_code, body = plivo.send_message message_params(dst_list, body)
+    raise PlivoClientError.new body.to_s, body.response unless status_code.to_s =~ /2\d\d/
+    status_code.to_s
   end
 
   private
@@ -20,5 +22,13 @@ module PlivoClient
 
   def plivo
     @plivo ||= RestAPI.new(PLIVO_CONFIG[:auth_id], PLIVO_CONFIG[:auth_token])
+  end
+end
+
+class PlivoClientError < StandardError
+  attr_accessor :status, :response
+  def initialize(status, response)
+    @status = status
+    @response = response
   end
 end
