9dc4220e827945b9f5dfab298fa419844fb81d56
Ryan Moser <ryanpmoser@gmail.com>
2014-02-22 15:08:56 -0800

    [TEST] verify linking messages clears contact notified timestamp

--- spec/lib/link_message_to_user_spec.rb
+++ spec/lib/link_message_to_user_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe LinkMessageToUser do
 
-  let(:contact) {FactoryGirl.create :contact, notified_at: 6.hours.ago}
+  let(:contact) {create :contact, notified_at: 6.hours.ago}
   let(:user) { contact.user}
-  let(:message) {FactoryGirl.build :inbound_message, from: user.phone_number}
+  let(:message) {build :inbound_message, from: user.phone_number}
 
   it 'sets the user_id of the message based on the phone number' do
     expect {
@@ -12,9 +12,8 @@ describe LinkMessageToUser do
     }.to change{message.user_id}.from(nil).to(user.id)
   end
 
-  it 'marks contacts as no longer notified' do
-    expect {
-      LinkMessageToUser.new(message).call
-    }.to change{contact.reload.notified?}.from(true).to(false)
+  it 'clears out the notified_at timestamp from the contact' do
+    LinkMessageToUser.new(message).call
+    expect(contact.reload.notified_at).to be_falsey
   end
 end
