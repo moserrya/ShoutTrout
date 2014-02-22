33a3180c9a4ef2d77b9427d6069b91d8c972778b
Ryan Moser <ryanpmoser@gmail.com>
2014-02-21 23:53:05 -0800

    [TEST] add a few user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) {User.new name: 'a', email: 'a@a.com', password: 'password', phone_number: '703-944-2823'}
+  it 'validates phone numbers' do
+    user.phone_number = '000'
+    expect(user.valid?).to be_falsey
+  end
+
+  specify 'the name is the users identify if present' do
+    expect(user.identity).to eq(user.name)
+  end
+
+  specify 'the email is the identity with no name present' do
+    user.name = nil
+    expect(user.identity).to eq (user.email)
+  end
 end
